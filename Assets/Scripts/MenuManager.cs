8a639647d12281b9e32d50ce64114a8669943816
Seidmani64 <A01028650@itesm.mx>
2022-10-20 17:20:56 -0500

    Finished button package

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,47 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
+[ExecuteInEditMode]
 public class MenuManager : MonoBehaviour
 {
-    public Button button;
-    public Button button2;
     public enum ButtonFunction {Select, Swap}
-    public ButtonFunction selectedFunction = ButtonFunction.Select;
+    [System.Serializable]
+    public struct MenuButton {
+     public Button button;
+     public ButtonFunction function;
+     public Button swapButton;
+                            }
+    [SerializeField]
+    public List<MenuButton> menuButtons;
+
     // Start is called before the first frame update
     void Start()
     {
-        button.onClick.AddListener(FirstTask);
-        button2.onClick.AddListener(SecondTask);
-    }
-
-    void FirstTask()
-    {
-        switch(selectedFunction)
-        {
-            case ButtonFunction.Select:
-                Debug.Log("Button 1 Selected.");
-                break;
-            case ButtonFunction.Swap:
-                button.gameObject.SetActive(false);
-                button2.gameObject.SetActive(true);
-                break;
-        }
+        if(menuButtons.Count > 0)
+            foreach(MenuButton mb in menuButtons)
+            {
+                if(mb.function == ButtonFunction.Select)
+                    mb.button.onClick.AddListener(delegate {Task(mb.button);});
+                else if(mb.swapButton == null)
+                    mb.button.onClick.AddListener(delegate {Task(mb.button, mb.button);});
+                else
+                    mb.button.onClick.AddListener(delegate {Task(mb.button, mb.swapButton);});
+                    
+            }
         
     }
 
-    void SecondTask()
+    void Task(Button b)
     {
-        switch(selectedFunction)
-        {
-            case ButtonFunction.Select:
-                Debug.Log("Button 2 Selected.");
-                break;
-            case ButtonFunction.Swap:
-                button.gameObject.SetActive(true);
-                button2.gameObject.SetActive(false);
-                break;
-        }
+        TextMeshProUGUI tmp = b.GetComponentInChildren<TextMeshProUGUI>();
+        Debug.Log(tmp.text + " Selected.");
+    }
+
+    void Task(Button b, Button b2)
+    {
+        b.gameObject.SetActive(false);
+        b2.gameObject.SetActive(true);
         
     }
 }
